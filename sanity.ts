66b5ff16356a2cac7d94bcb8a05939bb61ccb144
Ali Altan <alialtan2000@gmail.com>
2023-01-26 11:29:17 +0300

    checking

--- sanity.ts
+++ sanity.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
-
+//dummy
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: "lpmyz5a1",
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
